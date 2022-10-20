b7c26528d744204570f937601e35e1050d2d451a
c2run <jose.cerda66@gmail.com>
2022-10-20 15:52:40 -0300

    Se agrega request routeIs para identificar los enlaces visitados coloreando en rojo el link. se agrega estilo color rojo sin subrayado al texto de la clase enlace

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,15 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Título por defecto')</title>
+    <style>
+        .active a{
+            color: red;
+            text-decoration: none;
+        }
+    </style>
 </head>
 <body>
     <h1>Layout</h1>
     <nav>
     <ul>
-    <li><a href="/">Home</a></li>
-    <li><a href="/acerca">About</a></li>
-    <li><a href="/portafolio">Portafolio</a></li>
-    <li><a href="/contacto">Contacto</a></li>
+    <li class="{{ request()->routeIs('home') ? 'active' : '' }}"><a href="/">Home</a></li>
+    <li class="{{ request()->routeIs('about') ? 'active' : '' }}"><a href="/acerca">About</a></li>
+    <li class="{{ request()->routeIs('portafolio') ? 'active' : '' }}"><a href="/portafolio">Portafolio</a></li>
+    <li class="{{ request()->routeIs('contacto') ? 'active' : '' }}"><a href="/contacto">Contacto</a></li>
     </ul>
     </nav>
     @yield('content')
