e34696c8f94dbd58d452749252058892efe87958
c2run <jose.cerda66@gmail.com>
2022-10-20 09:05:10 -0300

    Se agrega un array con datos para pasarlo a la vista portafolio y acceder a este con estructura de control foreach de blade

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -4,4 +4,9 @@ Portafolio
 @endsection
 @section('content')
     <h1>Portafolio</h1>
+    <ul>
+    @foreach ($portafolio as $item)
+          <li>  {{ $item['title'] }} </li>
+    @endforeach 
+    </ul>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,14 @@ Route::get('/', function(){
     return view('home');
 })->name('home');
 */
+$portafolio = [
+    ['title' => 'Proyecto #1'],
+    ['title' => 'Proyecto #2'],
+    ['title' => 'Proyecto #3'],
+    ['title' => 'Proyecto #4'],
+];
+
 Route::view('/', 'home')->name('home');
 Route::view('/acerca', 'about')->name('about');
-Route::view('/portafolio', 'portfolio')->name('portfolio');
+Route::view('/portafolio', 'portfolio', compact('portafolio'))->name('portfolio');
 Route::view('/contacto', 'contact')->name('contacto');
