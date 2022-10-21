1d2f8cec34ba58e176308374f51e30564adca0e5
c2run <jose.cerda66@gmail.com>
2022-10-21 16:23:50 -0300

    Se agregan reglas de validación para campos de formulario en controlador. se agrega propiedad old para formulario para conservar textos después de error. se muestran mensajes de error con variable error

--- app/Http/Controllers/MensajesController.php
+++ app/Http/Controllers/MensajesController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 class MensajesController extends Controller
 {
     public function store(Request $request){
-        return $request;
+        return $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'asunto' => 'required',
+            'content' => 'required',
+        ]);
+        return 'Datos validados';
     }
 }
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,15 +4,20 @@ Contacto
 @endsection
 @section('content')
     <h1>Contacto</h1>
+    @if ($errors->any())
+       @foreach ($errors->all() as $error)
+           <p> {{ $error }} </p>
+       @endforeach
+    @endif
     <form action="{{route('contacto')}}" method="POST">
         @csrf
-        <input name="name" placeholder="Nombre..." type="text">
+        <input name="name" placeholder="Nombre..." type="text" value="{{old('name')}}">
         <br>
-        <input name="email" placeholder="Email..." type="email">
+        <input name="email" placeholder="Email..." type="email" value="{{old('email')}}">
         <br>
-        <input name="name" placeholder="Asunto..." type="text">
+        <input name="asunto" placeholder="Asunto..." type="text" value="{{old('asunto')}}">
         <br>
-        <textarea name="content" id="" cols="30" rows="10" placeholder="Mensaje..."></textarea>
+        <textarea name="content" id="" cols="30" rows="10" placeholder="Mensaje..." value="{{old('content')}}"></textarea>
         <br>
         <button>Enviar</button>
     </form>
